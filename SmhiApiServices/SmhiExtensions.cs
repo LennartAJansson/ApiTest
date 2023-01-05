5e6ca9940ed8b941df82ebc9ce837a5cdba75eff
Lennart Jansson <lennart.jansson@sogeti.com>
2023-01-05 21:18:36 +0100

    added services

---
+++ SmhiApiServices/SmhiExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace SmhiApiServices;
+
+using Contracts;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Refit;
+
+public static class SmhiExtensions
+{
+    public static IServiceCollection AddSmhiSupport(this IServiceCollection services, Func<IConfiguration> GetConfiguration)
+    {
+        IConfiguration config = GetConfiguration?.Invoke() 
+            ?? throw new ArgumentException("You have to point out the configuration where the base urls are");
+        
+        _ = services.AddRefitClient<ISmhiObservationApiClient>()
+            .ConfigureHttpClient(c =>
+            {
+                c.BaseAddress = new Uri(config["BaseUrls:SmhiObservation"]);
+                //If you need any headers or authentications added to your requests
+                //(it's also possible to add them as attributes to the interface or its methods):
+                //c.DefaultRequestHeaders.Add("user-agent", "News-API-csharp/0.1");
+                //c.DefaultRequestHeaders.Add("x-api-key", YourApiKey);
+            });
+        _ = services.AddRefitClient<ISmhiForecastApiClient>()
+            .ConfigureHttpClient(c =>
+            {
+                c.BaseAddress = new Uri(config["BaseUrls:SmhiForecast"]);
+                //If you need any headers or authentications added to your requests
+                //(it's also possible to add them as attributes to the interface or its methods):
+                //c.DefaultRequestHeaders.Add("user-agent", "News-API-csharp/0.1");
+                //c.DefaultRequestHeaders.Add("x-api-key", YourApiKey);
+            });
+        services.AddTransient<ISmhiForecastServices, SmhiForecastServices>();
+        services.AddTransient<ISmhiObservationServices, SmhiObservationServices>();
+        return services;
+    }
+}
