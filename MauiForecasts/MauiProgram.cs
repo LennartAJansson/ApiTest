787e68d12bc2ab9cf80b3681c999829393c037af
Lennart Jansson <lennart.jansson@sogeti.com>
2023-02-15 15:10:27 +0100

    Added configuration embedded for supporting Android

--- MauiForecasts/MauiProgram.cs
+++ MauiForecasts/MauiProgram.cs
@@ -1,11 +1,14 @@
 ﻿namespace MauiForecasts;
 
+using System.Reflection;
+
 using CommunityToolkit.Maui;
 using CommunityToolkit.Maui.Markup;
 
 using MauiForecasts.ViewModels;
 
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 using SmhiApiServices;
@@ -26,8 +29,13 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-            
-        var assembly = typeof(App).GetTypeInfo().Assembly;
+
+#if __ANDROID__
+#endif
+#if WINDOWS10_0_17763_0_OR_GREATER
+#endif
+
+        Assembly assembly = typeof(App).GetTypeInfo().Assembly;
         builder.Configuration.AddJsonFile(new EmbeddedFileProvider(assembly), "appsettings.json", optional: false, false);
         //builder.Configuration.AddJsonFile("appsettings.json");
         builder.Services.AddTransient<MainPage, MainViewModel>();
